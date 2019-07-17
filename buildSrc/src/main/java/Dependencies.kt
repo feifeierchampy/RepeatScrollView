cd383cbc4d2cb1fc1d4061d3b43100abc28bd04c
liuchang <liuchang06@kuaishou.com>
2019-07-17 18:35:04 +0800

    init

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,9 @@
+object Versions {
+  val supportLib = "27.0.0"
+}
+
+object Libs {
+  val appCompatV7 = "com.android.support:appcompat-v7:${Versions.supportLib}"
+  val recyclerView = "com.android.support:recyclerview-v7:${Versions.supportLib}"
+  val constraintLayout = "com.android.support.constraint:constraint-layout:1.1.3"
+}
